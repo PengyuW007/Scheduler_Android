7d52d69c32e79bc611bc6b9e7349b6502f0b6952
Skadoosh777 <670641576@qq.com>
2022-08-07 21:32:51 -0500

    view holder of seven days

--- app/src/main/java/com/example/scheduler/presentation/WeekTimeAdapter.java
+++ app/src/main/java/com/example/scheduler/presentation/WeekTimeAdapter.java
@@ -24,6 +24,7 @@ public class WeekTimeAdapter extends BaseAdapter {
         this.context = context;
         this.dataList = dataList;
         this.timeImage = timeImage;
+        this.inflater =(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }//end WeekTimeAdapter constructor
 
     public void setDataList(ArrayList<String[]> dL) {
@@ -71,10 +72,123 @@ public class WeekTimeAdapter extends BaseAdapter {
         String saturday = dataList.get(pos)[5];
         String sunday = dataList.get(pos)[6];
 
-        if(monday.equals("0")){
-            //viewHolder.one.setBackgroundResource(R.mipmap.);
+        for(int i = 0;i<7;i++){
+            String day="";
+            TextView num=null;
+            if(i==0){
+                day = monday;
+                num = viewHolder.one;
+            }else if(i==1){
+                day = tuesday;
+                num = viewHolder.two;
+            }else if(i==2){
+                day = wednesday;
+                num = viewHolder.three;
+            }else if(i==3){
+                day = thursday;
+                num = viewHolder.four;
+            }else if(i==4){
+                day = friday;
+                num = viewHolder.five;
+            }else if(i==5){
+                day = saturday;
+                num = viewHolder.six;
+            }else if(i==6){
+                day = sunday;
+                num = viewHolder.seven;
+            }
+
+            if(day.equals("0")){
+                num.setBackgroundResource(R.mipmap.btn_blockblue);
+                num.setText("am");
+            }else if (day.equals("1")){
+                num.setBackgroundResource(R.mipmap.btn_blockbrown);
+                num.setText("pm");
+            }else if(day.equals("2")){
+                num.setBackgroundResource(R.mipmap.btn_blocknight);
+                num.setText("midnight");
+            }
         }
-        return null;
+
+        /*
+        if(monday.equals("0")){
+            viewHolder.one.setBackgroundResource(R.mipmap.btn_blockblue);
+            viewHolder.one.setText("am");
+        }else if (monday.equals("1")){
+            viewHolder.one.setBackgroundResource(R.mipmap.btn_blockbrown);
+            viewHolder.one.setText("pm");
+        }else if(monday.equals("2")){
+            viewHolder.one.setBackgroundResource(R.mipmap.btn_blocknight);
+            viewHolder.one.setText("midnight");
+        }
+
+        if(tuesday.equals("0")){
+            viewHolder.two.setBackgroundResource(R.mipmap.btn_blockblue);
+            viewHolder.two.setText("am");
+        }else if (tuesday.equals("1")){
+            viewHolder.two.setBackgroundResource(R.mipmap.btn_blockbrown);
+            viewHolder.two.setText("pm");
+        }else if(tuesday.equals("2")){
+            viewHolder.two.setBackgroundResource(R.mipmap.btn_blocknight);
+            viewHolder.two.setText("midnight");
+        }
+
+        if(wednesday.equals("0")){
+            viewHolder.three.setBackgroundResource(R.mipmap.btn_blockblue);
+            viewHolder.three.setText("am");
+        }else if (wednesday.equals("1")){
+            viewHolder.three.setBackgroundResource(R.mipmap.btn_blockbrown);
+            viewHolder.three.setText("pm");
+        }else if(wednesday.equals("2")){
+            viewHolder.three.setBackgroundResource(R.mipmap.btn_blocknight);
+            viewHolder.three.setText("midnight");
+        }
+
+        if(thursday.equals("0")){
+            viewHolder.four.setBackgroundResource(R.mipmap.btn_blockblue);
+            viewHolder.four.setText("am");
+        }else if (thursday.equals("1")){
+            viewHolder.four.setBackgroundResource(R.mipmap.btn_blockbrown);
+            viewHolder.four.setText("pm");
+        }else if(thursday.equals("2")){
+            viewHolder.four.setBackgroundResource(R.mipmap.btn_blocknight);
+            viewHolder.four.setText("midnight");
+        }
+
+        if(friday.equals("0")){
+            viewHolder.five.setBackgroundResource(R.mipmap.btn_blockblue);
+            viewHolder.five.setText("am");
+        }else if (friday.equals("1")){
+            viewHolder.five.setBackgroundResource(R.mipmap.btn_blockbrown);
+            viewHolder.five.setText("pm");
+        }else if(friday.equals("2")){
+            viewHolder.five.setBackgroundResource(R.mipmap.btn_blocknight);
+            viewHolder.five.setText("midnight");
+        }
+
+        if(saturday.equals("0")){
+            viewHolder.six.setBackgroundResource(R.mipmap.btn_blockblue);
+            viewHolder.six.setText("am");
+        }else if (saturday.equals("1")){
+            viewHolder.six.setBackgroundResource(R.mipmap.btn_blockbrown);
+            viewHolder.six.setText("pm");
+        }else if(saturday.equals("2")){
+            viewHolder.six.setBackgroundResource(R.mipmap.btn_blocknight);
+            viewHolder.six.setText("midnight");
+        }
+
+        if(sunday.equals("0")){
+            viewHolder.seven.setBackgroundResource(R.mipmap.btn_blockblue);
+            viewHolder.seven.setText("am");
+        }else if (sunday.equals("1")){
+            viewHolder.seven.setBackgroundResource(R.mipmap.btn_blockbrown);
+            viewHolder.seven.setText("pm");
+        }else if(sunday.equals("2")){
+            viewHolder.seven.setBackgroundResource(R.mipmap.btn_blocknight);
+            viewHolder.seven.setText("midnight");
+        }
+         */
+        return convertView;
     }
 
     private class ViewHolder {
